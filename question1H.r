6544027365fc9879325421d7c6d6b009584173b1
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-29 17:49:10 -0300

    Update question1H.r

--- question1H.r
+++ question1H.r
@@ -6,6 +6,9 @@ file <- read.csv("table.csv",TRUE,sep = ",")
 altura<- hist(file$Altura, main="Frequência de altura", xlab = "Altura", ylab = "Quantidade de Pessoas")
  # Display weight histogram
 peso<- hist(file$Peso, main="Frequência de peso", xlab = "Peso", ylab = "Quantidade de Pessoas")
+ # Display IMC histogram
+imc<-hist(file$IMC, main="Frequência de IMC", xlab = "IMC", ylab = "Quantidade de Pessoas")
 
 print(altura)
 print(peso)
+print(imc)
