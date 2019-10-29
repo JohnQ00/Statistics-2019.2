590fc4d6159ef3c94f87ce5e7643786eeca62bd6
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-29 17:23:52 -0300

    Create code.r

---
+++ code.r
@@ -0,0 +1,13 @@
+# Display table
+file <- read.csv("table.csv",TRUE,sep = ",")
+print(file)
+
+# Display specific column
+print(file$Nome)
+print(file$Altura.m.)
+
+# Also an option
+file_peso <- file$Peso.kg.
+print(file_peso)
+
+#Créditos à Luana Júlia Nunes Ferreira
