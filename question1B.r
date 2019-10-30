614ea734f8da3a3d1bac7ce68bc1f3eb88bc6ca1
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-29 22:28:04 -0300

    Update question1B.r

--- question1B.r
+++ question1B.r
@@ -1,6 +1,7 @@
 # Display the table
 file <- read.csv("tablej.csv",TRUE,sep = ",")
 print(file)
+file$IMC<- (file$Peso/(file$Altura)^2)
 
  # Display the absolute frequency 
 freq_imc <- table(file$IMC)
