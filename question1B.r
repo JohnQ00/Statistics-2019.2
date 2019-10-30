077b8f5ccf8bbf29f81466fbf5865d12041ca974
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-29 21:50:47 -0300

    Update question1B.r

--- question1B.r
+++ question1B.r
@@ -1,1 +1,16 @@
+# Display the table
+file <- read.csv("tablej.csv",TRUE,sep = ",")
+print(file)
 
+ # Display the absolute frequency 
+freq_imc <- table(file$IMC)
+print(freq_imc)
+freq_peso <- table(file$Peso)
+print(freq_peso)
+freq_altura <- table(file$Altura)
+print(freq_altura)
+ # Display the relative frequency
+freq_rel <- prop.table(freq_imc)
+print(freq_rel)
+ # Display the percentage of relative frequency
+print(100 * freq_rel)
