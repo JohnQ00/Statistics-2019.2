804b464fc01788d033bcfb7bd330094a007450ac
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-29 22:09:30 -0300

    Update question1B.r

--- question1B.r
+++ question1B.r
@@ -14,3 +14,8 @@ freq_rel <- prop.table(freq_imc)
 print(freq_rel)
  # Display the percentage of relative frequency
 print(100 * freq_rel)
+ # Display the table of frequencies
+> brk<-seq(18,30,3)
+> print(brk)
+> classes<-c("18-20","21-23","24-26","27-30")
+> table(cut(file$IMC,breaks=brk,right=FALSE,labels=classes))
