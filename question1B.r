89ff69cb860e970f2a1bf12dbc0890e2dfb6ef36
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-31 13:04:52 -0300

    Update question1B.r

--- question1B.r
+++ question1B.r
@@ -16,7 +16,7 @@ print(freq_rel)
  # Display the percentage of relative frequency
 print(100 * freq_rel)
  # Display the table of frequencies
-> brk<-seq(18,30,3)
-> print(brk)
-> classes<-c("18-20","21-23","24-26","27-30")
-> table(cut(file$IMC,breaks=brk,right=FALSE,labels=classes))
+brk<-seq(18,30,3)
+print(brk)
+classes<-c("18-20","21-23","24-26","27-30")
+table(cut(file$IMC,breaks=brk,right=FALSE,labels=classes))
